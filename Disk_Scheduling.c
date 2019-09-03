4e10781f3548307b70b7fcdce96355652d610e05
Megha K.C <meghakc10@gmail.com>
2019-09-04 05:06:13 +0530

    SCAN

--- Disk_Scheduling.c
+++ Disk_Scheduling.c
@@ -32,7 +32,57 @@ main()
 				printf("Total head movements = %d cylinders\n", tot);
 				break;
 		
-		case 2: break;
+		case 2: //choosing default direction towards right
+				f = 0;
+				for (i = 0; i < n-1; i++)
+				{
+					p = i;
+					for (j = i+1; j < n; j++)
+					{
+						if(req[j]<req[p])
+						{
+							p = j;
+						}
+					}
+					t = req[i];
+					req[i] = req[p];
+					req[p] = t;
+					if(f==0 && req[p]>=head)
+					{
+						f = 1;
+						small = p;
+					}
+				}
+				for (i = 0; i < n; i++)
+					printf("%d\n", req[i] );
+				// printf("Small = %d\n", small);
+				// i=small;
+				j = i-1;
+				printf("\nHEAD MOVEMENTS:\n");
+				printf("%d->", head);
+				prev = head;
+				while(i<n)
+				{
+					tot+=req[i]-prev;
+					prev = req[i];
+					printf("%d->", req[i]);
+					i++;
+				}
+				printf("%d->", max);
+				tot+=max-req[i-1];
+				prev = max;
+				while(j>=0)
+				{
+					tot+= prev - req[j];
+					prev = req[j];
+					if(j==0)
+						printf("%d\n", req[j]);
+					else
+						printf("%d->", req[j]);
+					j--;
+				}
+				printf("Total head movements = %d cylinders\n", tot);
+				break;
 
 		case 3: break;
 		default: exit(0);
