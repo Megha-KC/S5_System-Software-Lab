0771ab9e77b5be14ae3eddc2ede7ec9b5a387f45
Megha K C <meghakc10@gmail.com>
2019-11-14 20:06:56 +0530

    Pass 1 of Two pass Macro processor

---
+++ Two pass Macro processor/macro_pass1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+
+main(){
+
+	FILE *f1,*f2,*f3;
+	f1 = fopen("input.txt","r");
+	f2 = fopen("NAMTAB","w+");
+	f3 = fopen("DEFTAB","w+");
+
+	char line[50],label[10],opcode[10],operand[10]; 
+
+	fscanf(f1,"%s%s%s", label,opcode,operand);
+	while(strcmp(opcode,"MEND")!=0)
+	{
+		if(strcmp(opcode,"MACRO")==0)
+		{
+			fprintf(f2,"%s\n",label);
+			fprintf(f3, "%s\t%s\n",label,operand);
+		}
+		else
+		{
+			fprintf(f3, "%s\t%s\n",opcode,operand);
+		}
+		fscanf(f1,"%s%s%s",label,opcode,operand);
+	}
+	fclose(f1);
+	fclose(f2);
+	fclose(f3);
+	printf("\nPass 1 of Two pass Macro processor is successful.\n");
+}
